c8b342a1462bab817ef69a9e99ff3d693f342188
trnKhanh <tnk29082003@gmail.com>
2024-09-13 13:23:23 +0700

    Update analyse and index: use "openai/clip-vit-large-patch14" as the default model

--- aic51/packages/webui/frontend/src/resources/options.js
+++ aic51/packages/webui/frontend/src/resources/options.js
@@ -1,4 +1,4 @@
 export const nlist = 128;
 export const nprobeOption = [8, 32, 128];
 export const limitOptions = [10, 20, 50];
-export const modelOptions = ["clip_b_16", "clip_b_32"];
+export const modelOptions = ["clip_l_14"];
